fd0d80d73f4b24be146b422bd905321ff2a3885f
GeoIvano <georgistoykovivanov@abv.bg>
2018-02-27 07:57:31 +0200

    all changes in the design
    Signed-off-by: GeoIvano <georgistoykovivanov@abv.bg>

--- assets/script/code.js
+++ assets/script/code.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
     $("#btn").click(function() {
         $.getJSON('https://jsonplaceholder.typicode.com/users', function(data) {
             $.each(data, function(num, user) {
-                $("#container").append(`<div>Position: ${(num + 1)}<br>Name :${user.name}<br> City : ${user.address.city} </div>`)
+                $("#container").append(`<li>Position: ${(num + 1)}<br>Name :${user.name}<br> City : ${user.address.city} </li>`)
             })
-            $("#btn").off("click")
+            $("#btn").off("click").css("cursor", "initial")
+
         })
     })
     $("#btn2").click(function() {
         $.get('https://jsonplaceholder.typicode.com/users', function(data) {
             $.each(data, function(i, person) {
-                $("#container2").append(`<div>Position: ${i+1}<br>Email: ${person.email}<br>Phone: ${person.phone}</div>`)
+                $("#container2").append(`<li>Position: ${i+1}<br>Email: ${person.email}<br>Phone: ${person.phone}</li>`)
             })
-            $("#btn2").off("click")
+            $("#btn2").off("click").css("cursor", "initial")
         })
     })
 })
